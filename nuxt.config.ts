1d439398f7370bb41ecf7fc106b4c4b4f73b2780
Micah Xia <1962408497@qq.com>
2023-10-12 11:35:19 +0800

    feat: 将 tailwindcss 替换为 unocss，引入 naive-ui 并设置自动导入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,23 @@
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+
 export default defineNuxtConfig({
   // @ts-ignore
   srcDir: 'src/',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  modules: ['@unocss/nuxt', '@pinia/nuxt'],
   css: ['~/assets/styles/common.css'],
   app: {
     head: {
       meta: [{ name: 'viewport', content: 'width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover' }]
     }
+  },
+  vite: {
+    plugins: [
+      Components({
+        resolvers: [NaiveUiResolver()]
+      })
+    ]
   },
   devServer: {
     host: 'localhost',
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,5 +7,26 @@ definePageMeta({
 <template>
   <div>
     <h1 class="text-3xl underline">Home: Hello World</h1>
+    <n-space>
+      <n-button>Default</n-button>
+      <n-button type="tertiary">
+        Tertiary
+      </n-button>
+      <n-button type="primary">
+        Primary
+      </n-button>
+      <n-button type="info">
+        Info
+      </n-button>
+      <n-button type="success">
+        Success
+      </n-button>
+      <n-button type="warning">
+        Warning
+      </n-button>
+      <n-button type="error">
+        Error
+      </n-button>
+    </n-space>
   </div>
 </template>
